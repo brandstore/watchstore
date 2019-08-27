f269b26ac5ec8ccd5939f3d6af94e2ce4b5069ba
pierre.c <pierre.c@kakaocorp.com>
2019-08-27 10:27:15 +0900

    second commit

---
+++ src/main/kotlin/house/brandstore/watchstore/WatchstoreApplication.kt
@@ -0,0 +1,11 @@
+package house.brandstore.watchstore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WatchstoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<WatchstoreApplication>(*args)
+}
---
+++ src/test/kotlin/house/brandstore/watchstore/WatchstoreApplicationTests.kt
@@ -0,0 +1,16 @@
+package house.brandstore.watchstore
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class WatchstoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
